b6f840990f9bcf9256e0a5898668954f82e3d1ef
Lâm Trần (Kaa) <123573848+LamTran0511@users.noreply.github.com>
2026-03-04 18:02:28 +0700

    Add files via upload

---
+++ console.sql
@@ -0,0 +1,49 @@
+create database IOC_MD02_Project;
+create schema if not exists projectioc;
+set search_path to projectioc;
+CREATE TABLE if not exists Admin(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(50)  NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS student (
+                                       id SERIAL PRIMARY KEY,
+                                       name VARCHAR(100) NOT NULL,
+                                       dob DATE NOT NULL,
+                                       email VARCHAR(100) NOT NULL UNIQUE,
+                                       sex BOOLEAN NOT NULL,          -- TRUE = Nam, FALSE = Nữ
+                                       phone VARCHAR(20),
+                                       password VARCHAR(255) NOT NULL,
+                                       create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS course (
+                                      id SERIAL PRIMARY KEY,
+                                      name VARCHAR(100) NOT NULL,
+                                      duration INT NOT NULL CHECK (duration > 0),  -- số giờ
+                                      instructor VARCHAR(100) NOT NULL,
+                                      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'enrollment_status') THEN
+            CREATE TYPE enrollment_status AS ENUM ('WAITING','DENIED','CANCEL','CONFIRMED');
+        END IF;
+    END
+$$;
+CREATE TYPE enrollment_status AS ENUM ('WAITING','DENIED','CANCEL','CONFIRMED');
+CREATE TABLE IF NOT EXISTS enrollment (
+                                          id SERIAL PRIMARY KEY,
+                                          student_id INT NOT NULL,
+                                          course_id INT NOT NULL,
+                                          registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                          status enrollment_status DEFAULT 'WAITING',
+
+                                          CONSTRAINT fk_enroll_student FOREIGN KEY (student_id)
+                                              REFERENCES student(id) ON DELETE CASCADE,
+
+                                          CONSTRAINT fk_enroll_course FOREIGN KEY (course_id)
+                                              REFERENCES course(id) ON DELETE CASCADE
+);
+-- (Gợi ý) tránh 1 học viên đăng ký trùng 1 khóa nhiều lần
+CREATE UNIQUE INDEX IF NOT EXISTS uq_enrollment_student_course
+    ON enrollment(student_id, course_id);
