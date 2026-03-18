76d5a8928ebc83e07a0baace1a74d7319f06b322
Lâm Trần (Kaa) <123573848+LamTran0511@users.noreply.github.com>
2026-03-19 00:04:13 +0700

    Delete IOC_MD02_Project directory

--- IOC_MD02_Project/service/IAdminService.java
+++
@@ -1,9 +0,0 @@
-package IOC_MD02_Project.service;
-
-import IOC_MD02_Project.model.Admin;
-
-import java.sql.SQLException;
-
-public interface IAdminService {
-    Admin login(String username, String password) throws SQLException;
-}
