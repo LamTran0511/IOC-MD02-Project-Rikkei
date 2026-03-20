bc8af64ab01b10b82fca424ec2bdc59a1f967ca3
Lâm Trần (Kaa) <123573848+LamTran0511@users.noreply.github.com>
2026-03-21 00:42:52 +0700

    Delete IOC_MD02_Project directory

--- IOC_MD02_Project/dao/IAdminDao.java
+++
@@ -1,9 +0,0 @@
-package IOC_MD02_Project.dao;
-
-import IOC_MD02_Project.model.Admin;
-
-import java.sql.SQLException;
-
-public interface IAdminDao {
-    Admin login(String username, String password) throws SQLException;
-}
